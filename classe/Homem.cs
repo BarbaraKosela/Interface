eca2ea6a676ae58c2477bd344e21ecf3a1dd040e
barbara.kosela <barbara@myrp.com.br>
2019-12-11 08:52:39 -0300

    criação de classes e passando parâmetros para estas

--- classe/Homem.cs
+++ classe/Homem.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace classe
 {
-    class Mulher : Pessoa
+    class Homem : Pessoa
     {
-        public Mulher(string nomeCompleto, Sexo sexo)
+        public Homem(string nomeCompleto, Sexo sexo)
         {
-            
+
         }
     }
 }
